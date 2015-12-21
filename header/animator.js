36369379ada14fb5438532d65ce69450e47386e9
prayas <prayasneupane1@gmail.com>
2015-12-21 23:15:06 +0545

    slider ok

--- header/animator.js
+++ header/animator.js
@@ -2,66 +2,61 @@ function Animator(element)
  {
 	this.el = element;
 	var that = this;
-
-	this.animate = function(value) 
+	var intervalId;
+	var left;
+	this.animation=false;
+	this.animate = function(value,step) 
 	{
 		var counter = 0;
-		var intervalId = setInterval(function() 
+		that.animation=true;
+		intervalId = setInterval(function() 
 		{
 			counter++;
-			//i++;
 			
-			
-			/*var current = step * i;
-			if(i>=40){
-				i=0;
-			
-			}
-			console.log(i);*/
-			//console.log(current);
-			// element.innerHTML = current;
-			console.log(i);
-			if(i==3)
-			{
-				element.style.left=counter*(-5)+((-(i-1))*100)+'%';;
-			}
-			else
-			{
-			element.style.left =counter*(-5)+((-(i-1))*100)+'%';
-			}//element.style[cssProperty]=current+'px';
-			//console.log(parseInt(style.getPropertyValue(cssProperty)) + step);
+			left=counter*(step)+((-(value))*100);
+
+			element.style.left =left+'%';
 			if (counter >= 20)
+			{
 				clearInterval(intervalId);
+				that.animation=false;
+			}
 		}, 50);
 	}
         
 			 
-		 this.stop = function()
-		 {
+	this.stop = function()
+	{
+		clearInterval(intervalId);
+		that.animation=false;
+				
 			
-	  
-		}
 
-		 
-      	this.animateBatch = function(properties, duration) {
-		for (var x in properties) {
-			console.log(x, properties[x]);
-		}
 	}
 
-	// should stop the animation in current position
+    // should stop the animation in current position
 	
 	// should stop the animation and element's properties should be at "end" value
-	this.finish = function() {
+	this.finish = function(value) 
+	{
+		
+		
+			var interval = setInterval(function()
+			{
+			if(left%100==0)
+			{
+				clearInterval(interval);
 
-	}
 
-	this.hitTest = function(singleElement) {
+				
+			}
+			else
+			{
+				left=left+value;
+				that.el.style.left=left+'%';
+			}
 
-	}
+			},1);
+	}	
 
-	this.hitTestBatch = function(elements) {
-
-	}
-
- }
+}
---
+++ header/slider.js
@@ -0,0 +1,73 @@
+ var screenWidth=window.innerWidth;
+		 var sliderWrapper=document.getElementsByClassName("slider-wrapper")[0];
+		 var sliderLong=document.getElementsByClassName("slider-long")[0];
+		 var slides=document.getElementsByClassName("slides");
+		 sliderLong.style.width=(100*slides.length)+'%';
+		 
+		 var percent=100/slides.length +'%';
+		 
+		 var screenHeight=window.innerHeight;
+		 sliderWrapper.style.height=screenHeight+'px';
+		 
+		 			slides[0].style.width=percent;
+			slides[1].style.width=percent;
+			slides[2].style.width=percent;
+			slides[3].style.width=percent;
+			slides[4].style.width=percent;
+			
+			var i=0;
+			var animator=new Animator(sliderLong);
+			function move()
+			{	if(i>slides.length-2) i=0;
+				var sliderLong=document.getElementsByClassName("slider-long")[0];
+			    animator.animate(i,-5);
+			    i++;
+				
+			}
+			var id=setInterval(move,3000);
+
+			function moveLeft()
+			{
+				if(i==0)
+				{
+				 i=slides.length-1; 
+				}
+				
+				window.clearInterval(id);
+				if(animator.animation==true)
+				{
+					animator.stop();
+					animator.finish(5);
+
+				}
+				else
+				{
+					animator.animate(i,5);
+				}
+				i=i-1;
+			    id= setInterval(move,3000);
+				
+			}
+			function moveRight()
+			{
+				 
+				if(i==slides.length-1) i=0;
+				window.clearInterval(id);
+				if(animator.animation==true)
+				{ 
+					animator.stop();
+					animator.finish(-5);
+					animator.animate(i,-5);
+				}
+				else
+				{
+					animator.animate(i,-5);
+				}
+		
+			   id= setInterval(move,3000);
+
+				i=i+1;	
+				
+			}
+		 
+		
